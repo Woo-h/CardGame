7b8be5a764abc3e838b9a70a18a52c9797212722
DESKTOP-DUN0RB0\rladn <rladndus3@gmail.com>
2022-01-15 20:07:44 +0900

    no message

---
+++ Test/src/com/min/edu/TestCalll.java
@@ -0,0 +1,10 @@
+package com.min.edu;
+
+public class TestCalll {
+
+	public static void main(String[] args) {
+		TestClass t = new TestClass();
+		t.adg();
+	}
+
+}
--- Test/src/com/min/edu/TestClass.java
+++ Test/src/com/min/edu/TestClass.java
@@ -4,5 +4,9 @@ public class TestClass {
 
 	public void adg() {
 		System.out.println();
+		
+		int a = 10;
+		
+		System.out.println(a);
 	}
 }
