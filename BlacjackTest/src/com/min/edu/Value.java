636c8e051cc48a0a228244bc50ed5ab3509bebc0
Woo-h <83409399+Woo-h@users.noreply.github.com>
2022-01-24 08:32:21 +0900

    Delete BlacjackTest directory

--- BlacjackTest/src/com/min/edu/Value.java
+++
@@ -1,8 +0,0 @@
-package com.min.edu;
-// 카드의 값도 역시 2부터 ACE까지 정해져 있기 때문에 상수를 다루는 enum을 사용.
-
-public enum Value {
-	
-	TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, KING, QUEEN, ACE
-
-}
